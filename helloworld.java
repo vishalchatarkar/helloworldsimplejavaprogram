6081c508b92dd26d2d12b49618c5ffa956e7cab7
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2022-10-07 08:46:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch27 team");
-      System.out.println("Hello Batch batch27 WITH FOR STUDENTS RUNNING in front of anil utkarsh meghna");
+      System.out.println("Hello Batch batch27 WITH FOR STUDENTS RUNNING in front of anil utkarsh meghna and i setup CI also at job 6 after that all job should run automatically");
     }  
 } 
