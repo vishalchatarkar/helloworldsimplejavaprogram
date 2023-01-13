18b929cdacc44e04f2188e569ad334ff45e94518
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2023-01-13 09:22:30 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch27 team kamlesh will do in some time in his company");
-      System.out.println("Hello Batch batch27.1 WITH FOR STUDENTS RUNNING in front of anil utkarsh meghna and i setup CI also at job 6 after that all job should run automatically");
+     System.out.println("Hello batch28 team Mriyank will do in some time in his company");
+      System.out.println("Hello Batch batch28 WITH FOR STUDENTS RUNNING in front of anil Aditya Suresh and i setup CI also at job 6 after that all job should run automatically");
     }  
 } 
