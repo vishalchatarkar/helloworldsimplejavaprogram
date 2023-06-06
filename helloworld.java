24982c15fb19ff199b8b4f13394c698fa8064279
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2023-06-06 08:52:16 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch29 team with Anu and team");
-      System.out.println("Hello Batch batch29 WITH FOR STUDENTS RUNNING in front of Advuth and hari and suchandra sindhu in making CI automated vikas teach devops CI proecss  ");
+     System.out.println("Hello batch30 team with joy , rushwanth  and team");
+      System.out.println("Hello Batch batch30 WITH FOR STUDENTS RUNNING in front of lion and Anita and suchandra sindhu in making CI automated vikas teach devops CI proecss  ");
     }  
 } 
