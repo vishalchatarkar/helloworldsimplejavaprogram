43965c1d8cb53de9df22afb05a2514905ffa34e6
vishalchatarkar <vishalpchatarkar7@gmail.com>
2025-02-03 15:38:40 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch 37 team with vishal,shubham,bhagyashri");
-      System.out.println("Hello Batch batch 38 WITH FOR STUDENTS RUNNING in front of me and vishal in making CI automated vikas teach devops CI proecss");
+      System.out.println("Hello Batch batch 38 WITH FOR STUDENTS RUNNING in front of me and vishal in making CI automated vikas teach devops CI process");
     }  
 } 
